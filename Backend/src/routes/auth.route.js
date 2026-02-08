88d65c79fe2bb003110402b34b0d90b5c61fcfd9
AbhishekX-dev <abhishekbandaswami@gmail.com>
2026-02-08 21:34:43 +0530

    user auth

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,17 +1,14 @@
 import express from "express"
+import { signup,login,logout,updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router=express.Router();
 
+router.post("/signup",signup )
 
-router.get("/signup" ,(req,res)=>{
-res.send("signup");
-})
+router.post("/login",login);
+ 
+router.post("/logout",logout)
 
-router.get("/login" ,(req,res)=>{
-res.send("login");
-})
-
-router.get("/logout" ,(req,res)=>{
-res.send("logout");
-})
+router.put("/update-profile",protectRoute,updateProfile);
 
 export default router;
