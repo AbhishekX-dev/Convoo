79ad64c38a88ab97fa4c6185058884bd6f99d098
AbhishekX-dev <abhishekbandaswami@gmail.com>
2026-02-08 21:36:21 +0530

    index file

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,23 @@
 import express from "express";
 import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+import dotenv from 'dotenv'
+import { ConnectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 const app= express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/auth",authRoutes);
 
-app.listen(5001, ()=>{
-    console.log("Server running");
+
+const PORT=process.env.PORT|| 3000;
+
+app.listen(PORT, ()=>{
+    console.log("Server running",PORT);
+    ConnectDB();
 });
 
